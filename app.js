7b9065f78615f41699a93fc0e62cc09449221a87
SuJiaKuan <jiakuan.su@toppano.in>
2018-06-13 00:42:12 +0800

    Add a basic express application

---
+++ app.js
@@ -0,0 +1,76 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const morgan = require('morgan');
+
+const routes = require('./routes');
+
+const PORT = 5000
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+
+// Cross origin.
+app.use(cors());
+
+// Logger.
+if (isProduction) {
+  app.use(morgan('common'));
+} else {
+  app.use(morgan('dev'));
+}
+
+// Body parse.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Add custom routes.
+app.use(routes);
+
+/// Catch 404 and forward to error handler.
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+
+  err.status = 404;
+
+  next(err);
+});
+
+if (!isProduction) {
+  // Development error handler.
+  // It will print stacktrace.
+  app.use(function(err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.send({
+      error: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+} else {
+  // Production error handler.
+  // No stacktraces leaked to user.
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+
+    res.send({
+      error: {
+        message: err.message,
+      },
+    });
+  });
+}
+
+
+// Start the server.
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error(err)
+  } else {
+    console.info(`==> Listening on port ${PORT}`)
+  }
+})
